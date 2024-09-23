af23cd39b61ddd247c431ec877e00d59dbf2e289
TechStackInb <coding@inboundsys.com>
2024-09-23 13:37:44 +0530

    update

--- src/model.js
+++ src/model.js
@@ -17,4 +17,5 @@ const memberSchema = new mongoose.Schema(
   }
 );
 
-module.exports = mongoose.model('Member', memberSchema);
+const Member = mongoose.model('Member', memberSchema);
+module.exports = Member;
